57d390e622d006303826b46f6ab211ced608da38
Kevin <kevin@sonny>
2026-04-10 17:19:20 -0700

    Change default Reminders list to 'Grocery'

--- lib/mealplan/store.ts
+++ lib/mealplan/store.ts
@@ -7,7 +7,7 @@ const HISTORY_KEY = "mealplan:shared:history";
 const PREFS_KEY = "mealplan:shared:prefs";
 
 const DEFAULT_PREFS: MealPlanPrefs = {
-  defaultRemindersListName: "Grocery List",
+  defaultRemindersListName: "Grocery",
 };
 
 export async function getActivePlan(): Promise<MealPlan | null> {
